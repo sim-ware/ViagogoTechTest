97de8e33d0b98882d02741ea719d15ba4fbf9ccd
Samir Gossain <samir.gossain@gmail.com>
2017-08-03 20:03:35 +0100

    Started to sketch out Event Class, going to focus on User Class instead

--- .pryrc
+++ .pryrc
@@ -1,3 +1,12 @@
 require './lib/coordinate'
+require './lib/event'
+
 a = Coordinate.new
 a.set(-11,11)
+
+b = Event.new
+b.id=('001')# way of limiting it to a three digit string
+b.price=('$34.50')# way of conducting it as a price
+b.tickets=('90')# way of making sure it is whole integer, can't be less than zero
+# b.coordinate - find a way to store a coordinate object
+# b.distance - find a way to calculate this based on the user's location
---
+++ lib/event.rb
@@ -0,0 +1,44 @@
+require_relative 'coordinate'
+
+class Event
+  attr_accessor :coordinate, :id, :tickets, :price, :distance
+
+  def initialize
+    @coordinate = 0
+    @id = ''
+    @tickets = 0
+    @price = 0
+    @distance = 0
+  end
+
+end
+
+# require_relative 'pricer'
+# require_relative 'discounter'
+#
+# class Order
+#   attr_accessor :clock, :standard_del, :express_del, :price, :discount_price
+#
+#   def initialize
+#     @clock = ''
+#     @standard_del = []
+#     @express_del = []
+#     @price = 0
+#     @discount_price = 0
+#   end
+#
+#   def clock_number(number)
+#     @clock = "#{number}"
+#   end
+#
+#   def standard_delivery_companies(*args)
+#     @standard_del.push(args)
+#     @standard_del = @standard_del[0]
+#   end
+#
+#   def express_delivery_companies(*args)
+#     @express_del << args
+#     @express_del = @express_del[0]
+#   end
+#
+# end
