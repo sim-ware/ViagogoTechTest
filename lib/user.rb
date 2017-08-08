48b8f48cd298f4c7607de0d143aa169e68bd3da6
Samir Gossain <samir.gossain@gmail.com>
2017-08-08 17:54:48 +0100

    Safety Commit

--- lib/user.rb
+++ lib/user.rb
@@ -13,6 +13,9 @@ class User
     @user_input = gets.chomp
   end
 
+  # strip input and reassemble inside take_user_input method?
+  # then we can have a simpler checker that works on trimmed variables instead of having to find them too
+
   def check_and_set_coordinates
     self.user_input.split(",")[0].to_s.strip.to_i >= -10 && self.user_input.split(",")[0].to_s.strip.to_i <= 10 ? self.coordinate.x = self.user_input.split(",")[0].to_s.strip : 'Enter Coordinates as Integers between -10 and 10'
     self.user_input.split(",")[1].to_s.strip.to_i >= -10 && self.user_input.split(",")[1].to_s.strip.to_i <= 10 ? self.coordinate.y = self.user_input.split(",")[1].to_s.strip : 'Enter Coordinates as Integers between -10 and 10'
